2aaf5183e08e0d53bc1d6e46820829490e45b518
Haislich <josedlv936@gmail.com>
2024-08-20 19:39:52 +0200

    updated dependencies, created face_Detection module

--- src/megatron/face_detection.py
+++ src/megatron/face_detection.py
@@ -1,7 +1,17 @@
-from torchvision import io
+import cv2 as cv
+import matplotlib.pyplot as plt
 
 
 def extract_frames():
     filepath = r"G:\My Drive\Megatron_DeepFake\dataset\manipulated_sequences\FaceShifter\raw\videos\033_097.mp4"
-    frames = io.read_video(filename=filepath)
-    print(frames)
+    capture_card = cv.VideoCapture(filepath)
+    while True:
+        ret, frame = capture_card.read()
+        if not ret:
+            break
+        plt.imshow(frame)
+        plt.show()
+    capture_card.release()
+
+
+extract_frames()
